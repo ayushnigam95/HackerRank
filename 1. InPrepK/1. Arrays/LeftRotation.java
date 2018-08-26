7d6e7295a4849098db20667c2176e1e3d688a57c
Ayush Nigam <ayush.nigam95@gmail.com>
2018-08-26 21:53:48 +0530

    Add files via upload

---
+++ 1. InPrepK/1. Arrays/LeftRotation.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+// CONCERNED AREA STARTS
+public class Solution {
+
+    // Complete the rotLeft function below.
+    static int[] rotLeft(int[] a, int d) {
+        int new_array[] = new int[a.length];
+        for(int i = 0; i < new_array.length; i++){
+            new_array[i] = a[d % a.length];
+            d++;
+        }
+        return new_array;
+        //System.out.print(a[(d % req_variable)] + " " + a[(d + 2) % req_variable]+ " " +a[(d + 3) % req_variable]+ " " +a[(d + 4) % req_variable]+ " " +a[(d + 5) % req_variable]);
+        
+    }
+// CONCERNED AREA ENDS HERE    
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nd = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nd[0]);
+
+        int d = Integer.parseInt(nd[1]);
+
+        int[] a = new int[n];
+
+        String[] aItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a[i] = aItem;
+        }
+
+        int[] result = rotLeft(a, d);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
